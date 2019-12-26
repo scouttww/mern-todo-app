ac505245309b680b1f0806c1a3807f097d3d70ff
Scout Wallace <scouttww@gmail.com>
2019-12-26 11:09:28 -0500

    added backend

--- frontend/src/components/todos-list.component.js
+++ frontend/src/components/todos-list.component.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class TodosList extends Component {
-    render() {
-        return (
-            <div>
-                <p>Welcome to Todos List Component!!</p>
-            </div>
-        )
-    }
+
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/todos/')
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Welcome to Todos List Component!!</p>
+      </div>
+    )
+  }
 }
